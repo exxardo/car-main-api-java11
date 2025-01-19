39ac57e11ee727f18ae75d8fab575bdd0a49779d
exxardo <eduardoramirodematos@gmail.com>
2025-01-19 12:49:57 -0300

    Initial commit

---
+++ src/main/java/com/carservice/carmainapi/CarMainApiApplication.java
@@ -0,0 +1,13 @@
+package com.carservice.carmainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarMainApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carservice/carmainapi/CarMainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carservice.carmainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMainApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
